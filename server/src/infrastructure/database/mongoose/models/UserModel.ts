2069f8720d6ba4b390c0f6839322fdfb97cc6f57
Suresh0750 <sureshvarma0750@gmail.com>
2024-09-27 05:20:00 -0700

    add a field in user model

--- server/src/infrastructure/database/mongoose/models/UserModel.ts
+++ server/src/infrastructure/database/mongoose/models/UserModel.ts
@@ -7,7 +7,8 @@ const userSchema = new Schema({
     EmailAddress : {type:String, required:true,unique:true},
     Address :{type : String,required :true},
     Password:{type : String,required:true},
-    isVerified : {type:Boolean, default:false}
+    isVerified : {type:Boolean, default:false},
+    isBlock : {type:Boolean,default:false}
 },{ timestamps: true })
 
 const UserModel = mongoose.model<User & Document>('UserDatas',userSchema)
