27c90c4991e10cbbb62abdb9f755e2bfaaa4525c
Suresh0750 <sureshvarma0750@gmail.com>
2024-10-18 05:52:33 -0700

    fix the bug in worker message page

--- client/src/components/worker/dashboard/Message.tsx
+++ client/src/components/worker/dashboard/Message.tsx
@@ -31,13 +31,8 @@ interface newMessage {
   _id: string;
   message: string;
   userId: any;
-<<<<<<< HEAD
   workerId: any,
   conversationId?:string
-=======
-  workerId: any;
-  conversationId? : string
->>>>>>> d3d9fcd4dc93ae2ac5d58347f0f2fd25291ff5cf
 }
 
 
@@ -66,20 +61,6 @@ interface newMessage {
   useEffect(()=>{
     if (socket) {
       socket.on("message", (newMessage: newMessage) => {
-<<<<<<< HEAD
-        console.log('newMessage')
-        if(newMessage.conversationId==conversationID){
-          setMessages((prevMessage:any)=>[...prevMessage,newMessage])
-        }
-        setCustomerDatails((prevConv:any)=>{
-          const result = prevConv?.map((conv)=>{
-            if(conv._id==newMessage?.conversationId){
-              console.log(conv)
-              console.log(conv?._id,'count',newMessage?.conversationId)
-              console.log(conv?.userUnread)
-              let inCount = conv?.userUnread+1
-              return {...conv,lastMessage:newMessage?.message,userUnread:inCount}
-=======
         if(newMessage?.conversationId == JSON.parse(localStorage.getItem('conversationId')||'')){
           setMessages((prevMessage:any)=>[...prevMessage,newMessage])
         }
@@ -89,7 +70,6 @@ interface newMessage {
               return {...conv,lastMessage:newMessage?.message,workerUnread:0}
             }else if(conv._id==newMessage?.conversationId && newMessage?.conversationId ){
               return {...conv,lastMessage:newMessage?.message,workerUnread:(conv?.workerUnread)+1}  
->>>>>>> d3d9fcd4dc93ae2ac5d58347f0f2fd25291ff5cf
             }
             return conv
           })
